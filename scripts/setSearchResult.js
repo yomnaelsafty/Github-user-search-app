18b36edbc66bb9e0cc9504a95b7e0033144fd64a
yomnaelsafty <140314499+yomnaelsafty@users.noreply.github.com>
2026-06-06 19:17:02 +0300

    Refactor setSearchResult to use map and join

--- scripts/setSearchResult.js
+++ scripts/setSearchResult.js
@@ -8,14 +8,12 @@ const setSearchResult = (data) => {
   } else if (data === null) {
     result = "";
   } else if (data?.length) {
-    data.map((item) => {
-      result += `
-    <article class ="card">
-       <img class ="img" loading ="lazy" src="${item.avatar_url}"/>
-       <h2 class ="name"> ${item.login}</h2>
-    </article>
-    `;
-    });
+    result = data.map((item) => `
+	    <article class ="card">
+	       <img class ="img" loading ="lazy" src="${item.avatar_url}"/>
+	       <h2 class ="name"> ${item.login}</h2>
+	    </article>
+	    `).join('');
   }
 
   cardsElements.innerHTML = result;
